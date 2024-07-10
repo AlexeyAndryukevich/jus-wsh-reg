32df36dab67d9244c81151adddcb674118622c3a
Alexey Andryukevich <alexey.andryukevich@gmail.com>
2024-07-11 02:44:18 +0300

    Add cors

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const PORT = 8080;
 const { Client } = require('pg');
 console.log('it works 111');
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
 const db = new Client({
     host: process.env.HOST,
     user: process.env.DB_USER,
