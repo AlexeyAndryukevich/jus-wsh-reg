c128b56bbbbb431ca074f39930fa3eee22cee9f7
Alexey Andryukevich <alexey.andryukevich@gmail.com>
2024-07-13 11:39:41 +0300

    Add registration post

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const app = require('express')();
+const express = require('express');
 const PORT = 8080;
-
+const bodyParser = require('body-parser')
 const { Client } = require('pg');
-console.log('it works 111');
+const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -24,19 +26,53 @@ const db = new Client({
 
 db.connect();
 
-app.listen(process.env.PORT || PORT);
+app.listen(PORT);
+console.log('it works 111');
 
-app.get('/registration', async (req, res) => {
-    const dbRes = await getWorkshops();
-    res.status(200).send(dbRes)
+// workshops
+app.get('/workshops', async (req, res) => {
+  try {
+    const dbRes = await db.query(`SELECT * FROM workshop`);
+    db.end;
+    res.status(200).send(dbRes.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 })
 
-async function getWorkshops(data, table, condition) {
-    try {
-      const res = await db.query(`SELECT * FROM workshop`);
-      db.end;
-      return res.rows;
-    } catch (err) {
-      console.error(err.stack);
+// registrations
+app.get('/registrations', async (req, res) => {
+  try {
+    const dbRes = await db.query(`SELECT * FROM public.workshopassingment where datetime >= now()::date`);
+    db.end;
+    res.status(200).send(dbRes.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+})
+
+app.post('/registrations', async (req, res) => {
+  try {
+    const reg = {
+      workshopId: req.body.workshopId,
+      memberId: req.body.memberId,
+      memberName: req.body.memberName,
+      deviceId: req.body.deviceId,
+      datetime: new Date()
     }
-}
+    console.log(reg);
+    const qr = `INSERT INTO public.workshopassingment
+      (workshopid, memberid, membername, deviceid, datetime)
+      VALUES( '${reg.workshopId}', '${reg.memberId}', '${reg.memberName}', '${reg.deviceId}', '${reg.datetime.toISOString()}');`
+    const dbRes = await db.query(qr);
+    db.end;
+    console.log(dbRes);
+    res.status(200).send(reg);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
+})
+
